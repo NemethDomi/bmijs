7bef6a1dd500eb62f9a41836d17b9858951f7949
unknown <Nmeth Dominik@RISZI.SULINET.LOCAL>
2024-03-20 10:51:37 +0100

    Kész

---
+++ testtomg.js
@@ -0,0 +1,9 @@
+function index() {
+    var m = document.getElementById("magas").value;
+    m = Number(m)
+    var t = document.getElementById("test").value;
+    t = Number(t)
+    m = m/100
+    var szamol = t/(m*m)
+    document.getElementById("kiir").innerHTML = Math.round(szamol * 100) / 100;;
+}
